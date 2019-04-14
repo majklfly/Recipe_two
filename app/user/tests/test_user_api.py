4f04af08a900d1d93276ba5a4b21cf65d90b79c9
Michal <majklfly@gmail.com>
2019-04-14 21:34:06 +0100

    Added manage user endpoint

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -8,7 +8,7 @@ from rest_framework import status
 
 CREATE_USER_URL = reverse('user:create')
 TOKEN_URL = reverse('user:token')
-# ME_URL = reverse('user:me')
+ME_URL = reverse('user:me')
 
 
 def create_user(**param):
@@ -94,37 +94,48 @@ class PublicUserApiTests(TestCase):
         self.assertNotIn('token', res.data)
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
 
-    # def test_retrieve_user_unauthorized(self):
-    #     """Test that authentication is required for users"""
-    #     res = self.client.get(ME_URL)
-    #
-    #     self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
+    def test_retrieve_user_unauthorized(self):
+        """Test that authentication is required for users"""
+        res = self.client.get(ME_URL)
+
+        self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
 
 
-# class PrivatUserApiTest(TestCase):
-#     """Test API request that require authentication"""
-#
-#     def setUp(self):
-#         self.user = create_user(
-#             email='test@mail.com',
-#             password='testass',
-#             name='Michal',
-#         )
-#         self.client = APIClient()
-#         self.client.force_authenticate(user=self.user)
-#
-#     def test_retrieve_profile_success(self):
-#         """Test retrieve profile for logged in used"""
-#         res = self.client.get(ME_URL)
-#
-#         self.assertEqual(res.status_code, status.HTTP_200_OK)
-#         self.assertEqual(res.data, {
-#             'name': self.user.name,
-#             'email': self.user.email,
-#         })
-#
-#     def test_post_me_not_allowed(self):
-#         """Test that post is not allowed on the me url"""
-#         res = self.client.post(ME_URL, {})
-#
-#         self.assertEqual(res.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
+class PrivatUserApiTest(TestCase):
+    """Test API request that require authentication"""
+
+    def setUp(self):
+        self.user = create_user(
+            email='test@mail.com',
+            password='testass',
+            name='Michal',
+        )
+        self.client = APIClient()
+        self.client.force_authenticate(user=self.user)
+
+    def test_retrieve_profile_success(self):
+        """Test retrieve profile for logged in used"""
+        res = self.client.get(ME_URL)
+
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(res.data, {
+            'name': self.user.name,
+            'email': self.user.email,
+        })
+
+    def test_post_me_not_allowed(self):
+        """Test that post is not allowed on the me url"""
+        res = self.client.post(ME_URL, {})
+
+        self.assertEqual(res.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
+
+    def test_update_user_profile(self):
+        """Test updating the user profile for authenticating user"""
+        payload = {'name': 'Michal', 'password': 'testass'}
+
+        res = self.client.patch(ME_URL, payload)
+
+        self.user.refresh_from_db()
+        self.assertEqual(self.user.name, payload['name'])
+        self.assertTrue(self.user.check_password(payload['password']))
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
