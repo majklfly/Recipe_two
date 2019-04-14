064279e1f44aa83d0ff3c120fd984d510146c9db
Michal <majklfly@gmail.com>
2019-04-14 20:26:24 +0100

    check Travis

--- app/app/settings.py
+++ app/app/settings.py
@@ -83,7 +83,7 @@ WSGI_APPLICATION = 'app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'database1',
         'USER': 'postgres',
         'PASSWORD': '12345',
--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -8,6 +8,7 @@ from rest_framework import status
 
 CREATE_USER_URL = reverse('user:create')
 TOKEN_URL = reverse('user:token')
+# ME_URL = reverse('user:me')
 
 
 def create_user(**param):
@@ -92,3 +93,38 @@ class PublicUserApiTests(TestCase):
         res = self.client.post(TOKEN_URL, {'email': 'one', 'password': ''})
         self.assertNotIn('token', res.data)
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+
+    # def test_retrieve_user_unauthorized(self):
+    #     """Test that authentication is required for users"""
+    #     res = self.client.get(ME_URL)
+    #
+    #     self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
+
+
+# class PrivatUserApiTest(TestCase):
+#     """Test API request that require authentication"""
+#
+#     def setUp(self):
+#         self.user = create_user(
+#             email='test@mail.com',
+#             password='testass',
+#             name='Michal',
+#         )
+#         self.client = APIClient()
+#         self.client.force_authenticate(user=self.user)
+#
+#     def test_retrieve_profile_success(self):
+#         """Test retrieve profile for logged in used"""
+#         res = self.client.get(ME_URL)
+#
+#         self.assertEqual(res.status_code, status.HTTP_200_OK)
+#         self.assertEqual(res.data, {
+#             'name': self.user.name,
+#             'email': self.user.email,
+#         })
+#
+#     def test_post_me_not_allowed(self):
+#         """Test that post is not allowed on the me url"""
+#         res = self.client.post(ME_URL, {})
+#
+#         self.assertEqual(res.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
